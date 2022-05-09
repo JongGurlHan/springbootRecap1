db4c462b73b0bcabb1f7af10d164dcad28684f2c
JG HAN <gojkhan13@gmail.com>
2022-05-09 21:35:28 +0900

    0509 rest api 설정

--- src/main/java/com/jghan/myhome/repository/BoardRepository.java
+++ src/main/java/com/jghan/myhome/repository/BoardRepository.java
@@ -3,7 +3,15 @@ package com.jghan.myhome.repository;
 import com.jghan.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> { //<모델명, PK의 타입>
+
+    //제목으로 게시글 검색
+    List<Board> findByTitle(String title);
+    
+    //제목 혹은 내용으로 검색
+    List<Board> findByTitleOrContent(String title, String content);
 
 
 
